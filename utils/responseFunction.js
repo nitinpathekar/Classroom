4a1c7f0562e3a29ad83b88e60cdb6490b2edf10d
Nitin <nitinsardarpathekar@gmail.com>
2025-09-19 10:10:13 +0530

    update project files

---
+++ utils/responseFunction.js
@@ -0,0 +1,10 @@
+const responseFunction = (res, status, message, data, ok) => {
+    res.status(status).json({
+        message,
+        data,
+        ok
+    })
+}
+
+module.exports = responseFunction;
+
